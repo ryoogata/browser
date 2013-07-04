25963e0b2efafcbf076aaceed1c95907cc12eb26
ryoogata <ryo.ogata@gmail.com>
2013-07-04 09:23:47 +0900

    CentOS をサポート

--- recipes/firefox.rb
+++ recipes/firefox.rb
@@ -12,14 +12,30 @@ execute "apt-get update" do
 end.run_action(:run) if node['platform_family'] == "ubuntu"
 
 
+# firefox のインストール
 package "firefox" do
 	action :install
 end
 
 
+# Selenium RC で発生するエラーの対応
 case node['platform']
 when "ubuntu"
 	link "/usr/bin/firefox-bin" do
 		to "/usr/lib/firefox/firefox"
 	end
+when "centos"
+	link "/usr/bin/firefox-bin" do
+		to "/usr/lib64/firefox/firefox"
+	end
+end
+
+
+# X転送でFirefoxを起動できない問題を解決
+case node['platform']
+when "centos"
+        execute "dbus-uuidgen" do
+                command "/bin/dbus-uuidgen >> /var/lib/dbus/machine-id"
+		creates "/var/lib/dbus/machine-id"
+        end
 end
